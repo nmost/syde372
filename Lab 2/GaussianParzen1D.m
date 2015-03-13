e77039579d343f8dc17b6b4a0712c26879fc1186
Victor Vucicevich (Vic Vu) <victorvuc@gmail.com>
2015-03-13 16:27:25 -0400

    Update GaussianParzen1D.m
    H is sigma

--- Lab 2/GaussianParzen1D.m
+++ Lab 2/GaussianParzen1D.m
@@ -1,4 +1,4 @@
-function [ pd ] = GaussianParzen1D( trainingData, range, h, sigma )
+function [ pd ] = GaussianParzen1D( trainingData, range, h)
 %UNTITLED5 Summary of this function goes here
 %   Detailed explanation goes here
 len_r = length(range);
@@ -7,7 +7,7 @@ pd = zeros(1,len_r);
 for i = 1:len_r
     sum1 = 0.0;
     for j = 1:len_t
-        sum1 = sum1 + (1/(sigma *sqrt(2*pi)))*exp(-0.5/sigma^2*((range(i)-trainingData(j))...
+        sum1 = sum1 + (1/(sqrt(2*pi)))*exp(-0.5*((range(i)-trainingData(j))...
             /h)^2)/h;
     end
     pd(i) = sum1/len_t;
