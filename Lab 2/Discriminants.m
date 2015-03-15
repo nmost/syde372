9e8bfd993c1e2af0c37f8eb421a1bc39fa969dce
Adam Wootton <ajwootto@uwaterloo.ca>
2015-03-14 21:00:42 -0400

    more comments

--- Lab 2/Discriminants.m
+++ Lab 2/Discriminants.m
@@ -15,13 +15,15 @@ nbAm = zeros(200);
 
 classDistances=[];
 
-
+%loop until both sets of points are empty
 while (~isempty(aPoints) && ~isempty(bPoints))
     nba=-1;
     nab=-1;
     correctA = zeros(200);
     correctB = zeros(200);
     
+    %loop until we find a random point that perfectly classifies either A
+    %or B
     while (nba ~= 0 && nab ~= 0)
         rowA = randsample(1:1:size(aPoints, 1),1);
         rowB = randsample(1:1:size(bPoints, 1),1);
@@ -36,12 +38,17 @@ while (~isempty(aPoints) && ~isempty(bPoints))
         nbb=0;
         nba=0;
         nab=0;
+        %loop through rows of aPoints
         for i = 1:size(aPoints, 1)
            if(MEDDecisionMetric(aPoints(i,:),za) < (MEDDecisionMetric(aPoints(i,:),zb)))
                naa = naa+1;
+               %store list of indexes of correctly classified points 
+               %use naa to keep track of where we are in correctA (and also
+               %how many correctly classified points there are)
                correctA(naa) = i;
            else
                nab = nab+1;
+               %same thing but for misclassified
                naBm(nab) = i;
            end
         end
@@ -56,16 +63,21 @@ while (~isempty(aPoints) && ~isempty(bPoints))
            end
         end
     end
+    
     %stores each 'good' discriminant in a row of G, like this:
     % [Ax Ay Bx By nab nba]
     G(j, :) = [za zb nab nba];
     if (nab == 0)
         i = 1;
+        %correctB was initialized as zeroes, loop until we reach unset
+        %(zero) values
         while(correctB(i) > 0)
             %remove every point that was correctly classified from the
             %sample set (as per instructions)
             %correctB contains indexes of correctly classified points.
             %After each removal index shifts down one, hence the -(i - 1)
+            %IE. we are looping through an array and removing items from it
+            %at the same time
             bPoints(correctB(i) - (i - 1),:)=[];
             i = i + 1;
         end
